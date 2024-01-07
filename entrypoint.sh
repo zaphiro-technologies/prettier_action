9dadc6e95cd810d17fc4308b5668d2dc56e48cde
Federico M. Facca <federico.facca@zaphiro.ch>
2024-01-07 12:42:57 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,11 +48,11 @@ if [ -n "$INPUT_PRETTIER_PLUGINS" ]; then
     echo "Installing prettier with plugins: $INPUT_PRETTIER_PLUGINS"
     case $INPUT_PRETTIER_VERSION in
         false)
-            ls -la $GITHUB_ACTION_PATH/node_modules/
+            sudo find / -type d -name "node_modules"
             npm install --save-dev --silent prettier $INPUT_PRETTIER_PLUGINS
             ;;
         *)
-            ls -la $GITHUB_ACTION_PATH/node_modules/
+            sudo find / -type d -name "node_modules"
             npm install --save-dev --silent prettier@$INPUT_PRETTIER_VERSION $INPUT_PRETTIER_PLUGINS
             ;;
     esac
